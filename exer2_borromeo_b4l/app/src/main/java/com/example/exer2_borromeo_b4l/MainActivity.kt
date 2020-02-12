49c98b697e8dcf82908dd6c8aeeb807854dc621a
MidnightMe <43721030+MidnightMe@users.noreply.github.com>
2020-02-12 22:38:04 +0800

    feat:added resetDisplay() added changing textview to changeDisplay()

--- exer2_borromeo_b4l/app/src/main/java/com/example/exer2_borromeo_b4l/MainActivity.kt
+++ exer2_borromeo_b4l/app/src/main/java/com/example/exer2_borromeo_b4l/MainActivity.kt
@@ -8,12 +8,14 @@ import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     lateinit var displayImage : ImageView
+    lateinit var displayText : TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         displayImage = findViewById(R.id.shown_img);
+        displayText = findViewById(R.id.shown_txt);
     }
 
     fun changeDisplay(view: View) {
@@ -33,5 +35,32 @@ class MainActivity : AppCompatActivity() {
         }
 
         displayImage.setImageResource(drawableResource)
+
+        if (randomInt === 1) {
+            displayText.text = getString(R.string.agent_p)
+        } else if (randomInt === 2) {
+            displayText.text = getString(R.string.candace)
+        } else if (randomInt === 3) {
+            displayText.text = getString(R.string.danny_fenton)
+        } else if (randomInt === 4) {
+            displayText.text = getString (R.string.doofenshmirtz)
+        } else if (randomInt === 5) {
+            displayText.text = getString(R.string.grim)
+        } else if (randomInt === 6) {
+            displayText.text = getString(R.string.kuzco)
+        } else if (randomInt === 7) {
+            displayText.text = getString(R.string.mr_krabs)
+        } else if (randomInt === 8) {
+            displayText.text = getString(R.string.patrick_star)
+        } else if (randomInt === 9) {
+            displayText.text = getString(R.string.sam_manson)
+        } else {
+            displayText.text = getString(R.string.taylor_hebert)
+        }
+    }
+
+    fun resetDisplay(view: View) {
+        displayImage.setImageResource(R.drawable.tv_logo)
+        displayText.text = getString(R.string.header_text)
     }
 }
